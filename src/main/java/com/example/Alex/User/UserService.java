023140f827885d88f290613a1988451b1a9f533c
felixmaina <karani.maina2010@gmail.com>
2022-05-04 23:59:07 +0300

    add  a service class

--- src/main/java/com/example/Alex/User/UserService.java
+++ src/main/java/com/example/Alex/User/UserService.java
@@ -1,31 +1,31 @@
-//package com.example.Alex.User;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class UserService {
-//    private final  UserRepository userRepository;
-//@Autowired
-//    public UserService(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-//
-//
-//    public List<Users> getUsers(){
-////        return List.of(new User(1,"karani@gmail.com","1234"));
-//   return userRepository.findAll();
-//    }
-//
-//    public void addNewUser(Users users) {
-//  Optional<Users> usersByEmail=userRepository.findUsersByEmail(users.getEmail());
-//if (usersByEmail.isPresent()){
-//    throw new IllegalStateException("email taken");
-//}// can add more validations
-//
-//userRepository.save(users);
-//    }
-//}
+package com.example.Alex.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final  UserRepository userRepository;
+@Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<Users> getUsers(){
+//        return List.of(new User(1,"karani@gmail.com","1234"));
+   return userRepository.findAll();
+    }
+
+    public void addNewUser(Users users) {
+  Optional<Users> usersByEmail=userRepository.findUsersByEmail(users.getEmail());
+if (usersByEmail.isPresent()){
+    throw new IllegalStateException("email taken");
+}// can add more validations
+
+userRepository.save(users);
+    }
+}
